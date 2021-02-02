a514b563ad12257bee8e3dac05691148ea553288
JavaidAhmadKhan <relaxjavaid@gmail.com>
2021-02-03 01:52:39 +0530

    adding Fleet list

---
+++ components/UserFleetsList/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FlatList, View } from "react-native";
+
+import UserFleetPreview from "../UserFleetPreview";
+import userWithFleets from "../../data/usersWithFleets";
+
+const UserFleetsList = () => {
+  return (
+    <View>
+      <FlatList
+        data={userWithFleets}
+        renderItem={({ item }) => <UserFleetPreview user={item} />}
+        horizontal
+      />
+    </View>
+  );
+};
+
+export default UserFleetsList;
